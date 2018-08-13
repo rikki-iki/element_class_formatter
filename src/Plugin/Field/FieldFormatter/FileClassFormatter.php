d38d90df5427869168cfd28d5d0160f62c2f0bbe
rikki_iki <rikki.bochow@gmail.com>
2018-08-13 11:23:02 +1000

    Expand file formatter to show size and type

--- src/Plugin/Field/FieldFormatter/FileClassFormatter.php
+++ src/Plugin/Field/FieldFormatter/FileClassFormatter.php
@@ -4,20 +4,22 @@ namespace Drupal\element_class_formatter\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\file\Plugin\Field\FieldFormatter\GenericFileFormatter;
+use Drupal\file\Plugin\Field\FieldFormatter\FileFormatterBase;
+use Drupal\Core\Template\Attribute;
+use Drupal\Core\Url;
 
 /**
- * Plugin implementation of the 'generic file with class' formatter.
+ * Plugin implementation of the 'file link with class' formatter.
  *
  * @FieldFormatter(
  *   id = "file_class",
- *   label = @Translation("Generic file (with class)"),
+ *   label = @Translation("File link (with class)"),
  *   field_types = {
  *     "file"
  *   }
  * )
  */
-class FileClassFormatter extends GenericFileFormatter {
+class FileClassFormatter extends FileFormatterBase {
 
   use ElementClassTrait;
 
@@ -25,7 +27,12 @@ class FileClassFormatter extends GenericFileFormatter {
    * {@inheritdoc}
    */
   public static function defaultSettings() {
-    return ElementClassTrait::elementClassDefaultSettings(parent::defaultSettings());
+    $default_settings = parent::defaultSettings() + [
+      'show_filesize' => '0',
+      'show_filetype' => '0',
+    ];
+
+    return ElementClassTrait::elementClassDefaultSettings($default_settings);
   }
 
   /**
@@ -34,6 +41,18 @@ class FileClassFormatter extends GenericFileFormatter {
   public function settingsForm(array $form, FormStateInterface $form_state) {
     $elements = parent::settingsForm($form, $form_state);
     $class = $this->getSetting('class');
+
+    $elements['show_filesize'] = [
+      '#title' => $this->t('Display the file size'),
+      '#type' => 'checkbox',
+      '#default_value' => $this->getSetting('show_filesize'),
+    ];
+
+    $elements['show_filetype'] = [
+      '#title' => $this->t('Display the file type'),
+      '#type' => 'checkbox',
+      '#default_value' => $this->getSetting('show_filetype'),
+    ];
 
     return $this->elementClassSettingsForm($elements, $class);
   }
@@ -44,6 +63,12 @@ class FileClassFormatter extends GenericFileFormatter {
   public function settingsSummary() {
     $summary = parent::settingsSummary();
     $class = $this->getSetting('class');
+    if ($size = $this->getSetting('show_filesize')) {
+      $summary[] = $this->t('Show file size');
+    }
+    if ($type = $this->getSetting('show_filetype')) {
+      $summary[] = $this->t('Show file type');
+    }
 
     return $this->elementClassSettingsSummary($summary, $class);
   }
@@ -52,11 +77,58 @@ class FileClassFormatter extends GenericFileFormatter {
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
-    $elements = parent::viewElements($items, $langcode);
-    $entities = $this->getEntitiesToView($items, $langcode);
+    $elements = [];
     $class = $this->getSetting('class');
 
-    return $this->setEntityClass($elements, $class, $entities);
+    foreach ($this->getEntitiesToView($items, $langcode) as $delta => $file) {
+      $file_entity = $file->_referringItem->getEntity();
+      // Get default link text.
+      $link_text = $file_entity->label();
+      $attributes = new Attribute();
+      $attributes->setAttribute('title', $file->getFilename());
+
+      // File meta data.
+      $file_type = strtoupper(pathinfo($file->getFilename(), PATHINFO_EXTENSION));
+      $file_size = format_size($file->getSize());
+      $mime_type = $file->getMimeType();
+      $attributes->setAttribute('type', $mime_type . '; length=' . $file->getSize());
+
+      // Classes for styling.
+      $classes = [
+        'file',
+        'file--mime-' . strtr($mime_type, ['/' => '-', '.' => '-']),
+        'file--' . file_icon_class($mime_type),
+        $class,
+      ];
+      $attributes->addClass($classes);
+
+      // Customise link text.
+      $show_filesize = $this->getSetting('show_filesize');
+      $show_filetype = $this->getSetting('show_filetype');
+      if ($show_filesize && $show_filetype) {
+        $link_text = $link_text . ' (' . $file_type . ', ' . $file_size . ')';
+      }
+      elseif ($show_filesize && !$show_filetype) {
+        $link_text = $link_text . ' (' . $file_size . ')';
+      }
+      elseif (!$show_filesize && $show_filetype) {
+        $link_text = $link_text . ' (' . $file_type . ')';
+      }
+
+      // Build URL.
+      $url = Url::fromUserInput(file_url_transform_relative(file_create_url($file->getFileUri())));
+
+      $elements[$delta] = [
+        '#type' => 'link',
+        '#title' => $link_text,
+        '#url' => $url,
+        '#attributes' => $attributes->toArray(),
+        '#cache' => [
+          'tags' => $file->getCacheTags(),
+        ],
+      ];
+    }
+    return $elements;
   }
 
 }
